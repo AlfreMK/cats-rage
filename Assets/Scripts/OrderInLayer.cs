dc2fc08e221a41cf8afde0aff8efe486fedb12f8
alfre <alfredo.medina@uc.cl>
2023-09-03 15:43:04 -0300

    setup project

---
+++ Assets/Scripts/OrderInLayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class OrderInLayer : MonoBehaviour
+{
+    SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sr.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour
+{
+    // -4.85 -1.75
+
+    static Vector2 LimitsY = new Vector2(-4.85f, -1.75f);
+
+    // Start is called before the first frame update
+    [SerializeField] private float verticalSpeed = 0.3f;
+    [SerializeField] private float horizontalSpeed = 1f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator animator;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    Vector2 control;
+    // Update is called once per frame
+    void Update()
+    {
+        control = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (control.x != 0)
+        {
+            sr.flipX = control.x < 0;
+        }
+        animator.SetBool("isWalking", control.magnitude != 0);
+
+        rb.velocity = new Vector2(control.x * horizontalSpeed, control.y * verticalSpeed);
+        transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, LimitsY.x, LimitsY.y));
+        
+    }
+}
