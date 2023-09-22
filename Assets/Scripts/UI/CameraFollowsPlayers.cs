d84551aa0e2e7eac9b0529576abd0d59aca76b58
acastill20 <acastio@uc.cl>
2023-09-22 01:22:57 -0300

    Cambios Finales

--- Assets/Scripts/UI/CameraFollowsPlayers.cs
+++ Assets/Scripts/UI/CameraFollowsPlayers.cs
@@ -9,6 +9,13 @@ public class CameraFollowsPlayers : MonoBehaviour
     private float LimitXRight = 100-8.5f;
     private Player player1;
     private Player player2;
+    private bool isFollowing = true; // Add a flag to control following
+    private bool firstScreen = true;
+    private bool bossAlive = false;
+    private GameObject[] enemiesFirstScreen;
+    private GameObject[] boss;
+    private float remainingEnemies;
+    private float remainingBoss;
     void Start()
     {
         player1 = GameManager.Instance.GetPlayer1Script();
@@ -19,19 +26,48 @@ public class CameraFollowsPlayers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!player1.getIsMounted() && !player2.getIsMounted())
-        {
-            Vector3 middlePoint = GameManager.Instance.GetAveragePlayerPosition();
-            // transform.position = new Vector3(middlePoint.x, 0, -10);
-            transform.position = new Vector3(Mathf.Clamp(middlePoint.x, LimitXLeft, LimitXRight), 0, -10);
-        }
+        enemiesFirstScreen = GameObject.FindGameObjectsWithTag("EnemiesFirstScreen");
+        remainingEnemies = enemiesFirstScreen.Length;
+
+        boss = GameObject.FindGameObjectsWithTag("Boss");
+        remainingBoss = boss.Length;
+
+        Vector3 middlePoint = GameManager.Instance.GetAveragePlayerPosition();
+
         if (player1.getIsMounted())
         {
-            transform.position = new Vector3(Mathf.Clamp(player2.transform.position.x, LimitXLeft, LimitXRight), 0, -10);
+            middlePoint = player2.transform.position;
         }
         if (player2.getIsMounted())
         {
-            transform.position = new Vector3(Mathf.Clamp(player1.transform.position.x, LimitXLeft, LimitXRight), 0, -10);
+            middlePoint = player1.transform.position;
+        }
+
+        if (remainingEnemies == 0 && firstScreen)
+        {
+            isFollowing = true;
+            firstScreen = false;
+            transform.position = new Vector3(Mathf.Clamp(18.3f, LimitXLeft, LimitXRight), 0, -10);
+        }
+        if (middlePoint.x > 18.3 && remainingEnemies > 0)
+        {
+            isFollowing = false;
+        }
+
+        //Boss
+        if (remainingBoss == 0 && bossAlive)
+        {
+            isFollowing = true;
+            bossAlive = false;
+        }
+        if (middlePoint.x > 48.4 && remainingBoss > 0)
+        {
+            isFollowing = false;
+        }
+
+        if (isFollowing)
+        {
+            transform.position = new Vector3(Mathf.Clamp(middlePoint.x, LimitXLeft, LimitXRight), 0, -10);
         }
     }
 }
